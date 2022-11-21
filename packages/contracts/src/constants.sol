0b47b0a51a85ace10602c43200a9011dc6658b53
bytes <therealbyt3s@gmail.com>
2022-11-21 18:35:09 +0000

    feat: add constants

---
+++ packages/contracts/src/constants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+uint256 constant gridId = uint256(keccak256("conway.entity.grid"));
+
+int64 constant gridDimX = 100;
+int64 constant gridDimY = 100;
