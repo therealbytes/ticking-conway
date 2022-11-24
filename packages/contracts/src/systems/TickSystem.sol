8e9db0e536ac6909f69cc67e2fc796121428d4b0
bytes <therealbyt3s@gmail.com>
2022-11-24 17:54:04 +0000

    feat(contracts): update state with new cells

--- packages/contracts/src/systems/TickSystem.sol
+++ packages/contracts/src/systems/TickSystem.sol
@@ -7,9 +7,12 @@ import { getAddressById } from "solecs/utils.sol";
 import { Coord } from "../types.sol";
 import { GridId, TickPredeployAddr, TicksPerCall } from "../constants.sol";
 import { Conway } from "../libraries/LibConway.sol";
+import { NewCellsComponent, ID as NewCellsComponentID } from "../components/NewCellsComponent.sol";
 import { ConwayStateComponent, ID as ConwayStateComponentID } from "../components/ConwayStateComponent.sol";
 import { DimensionsComponent, ID as DimensionsComponentID } from "../components/DimensionsComponent.sol";
 import { CellBitSizeComponent, ID as CellBitSizeComponentID } from "../components/CellBitSizeComponent.sol";
+
+// import "forge-std/console.sol";
 
 uint256 constant ID = uint256(keccak256("conway.system.tick"));
 
@@ -32,11 +35,37 @@ contract TickSystem is System {
     CellBitSizeComponent cellBitSizeComponent = CellBitSizeComponent(
       getAddressById(components, CellBitSizeComponentID)
     );
+    NewCellsComponent newCellsComponent = NewCellsComponent(getAddressById(components, NewCellsComponentID));
     ConwayStateComponent conwayComponent = ConwayStateComponent(getAddressById(components, ConwayStateComponentID));
     // Get values
     Coord memory dimensions = dimensionsComponent.getValue(entity);
     uint256 cellBitSize = cellBitSizeComponent.getValue(entity);
     bytes memory state = conwayComponent.getValue(entity);
+    bytes memory newCells = newCellsComponent.getValue(entity);
+    // Update state with new cells
+    for (uint256 ii = 0; ii < newCells.length; ii++) {
+      bytes1 n = newCells[ii];
+      if (n == 0) {
+        continue;
+      }
+      bytes1 b = state[ii];
+      if (b == 0) {
+        state[ii] = n;
+        continue;
+      }
+      for (uint256 jj = 0; jj < 8 / cellBitSize; jj++) {
+        uint256 offset = jj * cellBitSize;
+        bytes1 mask = bytes1(uint8(2**cellBitSize - 1)) << (8 - offset - cellBitSize);
+        bytes1 sn = n & mask;
+        if (sn == 0) {
+          continue;
+        }
+        b = (b & ~mask) | sn;
+      }
+      state[ii] = b;
+    }
+    // Reset new cells
+    newCellsComponent.setValue(entity, new bytes(newCells.length));
     // Execute steps setting state every time
     for (uint256 ii = 0; ii < TicksPerCall; ii++) {
       state = Conway.step(uint256(int256(dimensions.x)), uint256(int256(dimensions.y)), cellBitSize, state);
