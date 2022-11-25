d42054f443fee45df50ecbe86a0c1237b43c7ca3
bytes <therealbyt3s@gmail.com>
2022-11-25 16:31:21 +0000

    refactor(client): clean

--- packages/client/src/layers/phaser/systems/createCanvasSystem.ts
+++ packages/client/src/layers/phaser/systems/createCanvasSystem.ts
@@ -4,6 +4,5 @@ import { Colors } from "../constants";
 export const createCanvasSystem = createGridSystemCreator("Canvas", {
   colors: [Colors.Blue],
   alphas: [0, 0.5],
-  frameTime: 0,
   depth: 1,
 });
