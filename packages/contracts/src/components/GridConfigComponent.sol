f84a106325f98f9a9220e21f7a154f7bcc38678b
bytes <therealbyt3s@gmail.com>
2022-11-27 11:54:49 +0000

    fix(contracts): add license and solc version

--- packages/contracts/src/components/GridConfigComponent.sol
+++ packages/contracts/src/components/GridConfigComponent.sol
@@ -1,5 +1,6 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
 import "solecs/BareComponent.sol";
-import { Coord } from "../types.sol";
 
 uint256 constant ID = uint256(keccak256("conway.component.gridConfig"));
 
