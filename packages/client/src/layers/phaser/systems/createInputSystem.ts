67f021df3d30430293440036130d5723f4ac4f86
bytes <therealbyt3s@gmail.com>
2022-11-27 13:20:50 +0000

    feat(client): only draw if drawable

--- packages/client/src/layers/phaser/systems/createInputSystem.ts
+++ packages/client/src/layers/phaser/systems/createInputSystem.ts
@@ -45,6 +45,7 @@ export function createInputSystem(network: NetworkLayer, phaser: PhaserLayer) {
         cellPos.y < config.posY + config.dimY
       ) {
         target = entity;
+        if (!config.drawable) return;
         cellInPos = { x: cellPos.x - config.posX, y: cellPos.y - config.posY };
         break;
       }
