73fd7ac247d97a65c7ed67503f1ea65934360850
bytes <therealbyt3s@gmail.com>
2022-12-06 18:00:57 +0000

    chore: no prod

--- packages/client/src/defaultConfig.ts
+++ packages/client/src/defaultConfig.ts
@@ -1,9 +1,9 @@
 export const defaultConfig = {
   worldAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
   chainIdString: "901",
-  jsonRpc: "https://l2.infra.conway.usebasic.dev",
-  wsRpc: "wss://l2.infra.conway.usebasic.dev",
-  snapshotServiceUrl: "https://ecs-snapshot.infra.conway.usebasic.dev",
-  streamServiceUrl: "https://ecs-stream.infra.conway.usebasic.dev",
-  faucetUrl: "https://faucet.infra.conway.usebasic.dev",
+  jsonRpc: "http://localhost:9545",
+  wsRpc: "ws://localhost:9546",
+  snapshotServiceUrl: "",
+  streamServiceUrl: "",
+  faucetUrl: "",
 };
