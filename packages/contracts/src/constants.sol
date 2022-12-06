4896ae1327d25d523bd6c91695af1e7f86ee55bf
bytes <therealbyt3s@gmail.com>
2022-12-06 17:31:39 +0000

    chore(contracts): set constants

--- packages/contracts/src/constants.sol
+++ packages/contracts/src/constants.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 uint256 constant GridId = 0x060D;
 uint8 constant GridStepsPerTick = 1;
 uint8 constant GridCellBitSize = 1;
-bool constant GridDrawable = false;
+bool constant GridDrawable = true;
 bool constant GridPausable = true;
 bool constant GridDevMode = true;
 int32 constant GridPosX = 0;
@@ -12,7 +12,7 @@ int32 constant GridPosY = 0;
 int32 constant GridDimX = 32;
 int32 constant GridDimY = 32;
 
-bool constant GridPaused = true;
-bool constant GridUsePrecompile = false;
+bool constant GridPaused = false;
+bool constant GridUsePrecompile = true;
 
 address constant TickPredeployAddr = 0x42000000000000000000000000000000000000A0;
